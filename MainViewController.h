23239dfec04f2caaa4ffed4c4a88538202140513
nA-kidka <R.WOLFMED@yandex.ru>
2022-11-29 07:27:35 +0300

    mega

---
+++ MainViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainViewController.h
+//  Meditation
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
